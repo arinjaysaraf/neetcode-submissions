bfcdf766d7e914b0c5142ae8bccaa89ae5513cb5
arinjay_s <56652667+arinjaysaraf@users.noreply.github.com>
2026-08-19 23:54:07 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int a = 0;
+        int b = n - 1;
+        if (nums[a] == target) return a;
+        if (nums[b] == target) return b;
+        for (int i = 0; i < n; i++) {
+            if (target < nums[(a + b) / 2])
+                b = (a + b) / 2;
+            else if (target > nums[(a + b) / 2])
+                a = (a + b) / 2;
+            else
+                return (a + b) / 2;
+        }
+        return -1;
+    }
+};
